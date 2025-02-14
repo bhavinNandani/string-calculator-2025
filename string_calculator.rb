59802db5d0b57b85880d41c06cd799285b27f97e
Bhavin Nandani <nandanibhavin@gmail.com>
2025-02-15 01:34:21 +0530

    code improvement

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,17 +1,29 @@
 class StringCalculator
+  DEFAULT_DELIMITERS = /[\n,]/
+
   def add(numbers)
     return 0 if numbers.empty?
 
-    delimiter = /[\n,]/
-    if numbers.start_with?("//")
-      delimiter = numbers[2..numbers.index("\n") - 1]
-      numbers = numbers[(numbers.index("\n") + 1)..]
-    end
-
+    delimiter, numbers = parse_delimiter(numbers)
     num_array = numbers.split(delimiter).map(&:to_i)
-    negatives = num_array.select(&:negative?)
-    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+
+    check_for_negatives(num_array)
 
     num_array.sum
+  end
+
+  private
+  def parse_delimiter(numbers)
+    return [DEFAULT_DELIMITERS, numbers] unless numbers.start_with?("//")
+
+    delimiter, numbers = numbers.split("\n", 2)
+    custom_delimiter = delimiter[2..]
+
+    [Regexp.escape(custom_delimiter), numbers]
+  end
+
+  def check_for_negatives(numbers)
+    negatives = numbers.select(&:negative?)
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
   end
 end
