6474188df09b011a5d5b557d6fa38cee88e3f38b
Bhavin Nandani <nandanibhavin@gmail.com>
2025-02-15 01:24:40 +0530

    newline support added

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -20,6 +20,11 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("1,2")).to eq(3)
     end
+
+    it "handles new lines as delimiters" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
   
 end
