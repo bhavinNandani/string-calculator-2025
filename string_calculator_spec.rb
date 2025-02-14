11dff7d7b478e0416e381985eaf7969fd565b793
Bhavin Nandani <nandanibhavin@gmail.com>
2025-02-15 01:19:48 +0530

    add method implemented with empty string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe StringCalculator do
   it "initializes the StringCalculator class" do
     expect { StringCalculator.new }.not_to raise_error
   end
+
+  context "#add" do
+    it "returns 0 for an empty string" do
+      calculator = StringCalculator.new
+      expect(calculator.add("")).to eq(0)
+    end
+  end
+  
 end
